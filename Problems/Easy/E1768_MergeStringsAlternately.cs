b13348efa621396c0ccf76ef467d1f1ea67043c9
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-03-12 21:43:44 -0400

    Improve e1768

--- Problems/Easy/E1768_MergeStringsAlternately.cs
+++ Problems/Easy/E1768_MergeStringsAlternately.cs
@@ -3,27 +3,34 @@
 namespace Problems;
 public class E1768_MergeStringsAlternately
 {
+    /// <summary>
+    /// https://leetcode.com/problems/merge-strings-alternately/description/
+    /// You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1.
+    /// If a string is longer than the other, append the additional letters onto the end of the merged string.
+    /// Return the merged string.
+    /// </summary>
     public static string MergeAlternately(string word1, string word2)
     {
-        StringBuilder stringBuilder = new();
-        (int, string) biggestWord = word1.Length > word2.Length ? (word1.Length - 1, word1) : (word2.Length - 1, word2);
-        (int, string) smallestWord = word1.Length < word2.Length ? (word1.Length - 1, word1) : (word2.Length - 1, word2);
-        var diff = biggestWord.Item1 - smallestWord.Item1;
+        StringBuilder result = new();
+        (string word, int index) upper = word1.Length > word2.Length ? (word1, word1.Length - 1) : (word2, word2.Length - 1);
+        (string word, int index) lower = word1.Length > word2.Length ? (word2, word2.Length - 1) : (word1, word1.Length - 1);
 
-        for (int i = 0; i <= biggestWord.Item1; i++)
+        for (int i = 0; i <= upper.index; i++)
         {
-            if (i > smallestWord.Item1)
+            if (i <= lower.index)
             {
-                for (int j = i; j < i + diff; j++)
-                {
-                    stringBuilder.Append(biggestWord.Item2[j]);
-                }
-                break;
+                result.Append(word1[i]);
+                result.Append(word2[i]);
+                continue;
             }
-            stringBuilder.Append(word1[i]);
-            stringBuilder.Append(word2[i]);
+            int charsRemaining = upper.index - lower.index;
+            for (int n = i; n < i + charsRemaining; n++)
+            {
+                result.Append(upper.word[n]);
+            }
+            break;
         }
 
-        return stringBuilder.ToString();
+        return result.ToString();
     }
 }
